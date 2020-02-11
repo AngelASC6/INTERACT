53f08e5f70fdecfbe0cd638b6fc0fc43ebafb04f
angelASC6 <angelmichaelvasquez@gmail.com>
2020-02-11 09:13:34 -0500

    new product page

--- script/createProductPage.js
+++ script/createProductPage.js
@@ -6,11 +6,6 @@ function queryDatabase(collectionName,refId,code) {
     db.collection(collectionName).doc(refId).get().then(code);   
 }
 
-queryDatabase('items','I7ddGrPK5nqcAMSlgCyf',function(response){
-    data = response.data()
-    displayContent(data)
-})
-
 
 function displayContent(response){
     console.log(response.title)
