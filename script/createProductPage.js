89d95b7a712dc78676b0abe3eba9a1f485e937df
angelASC6 <angelmichaelvasquez@gmail.com>
2020-02-13 13:03:11 -0500

    Efficient local storage

--- script/createProductPage.js
+++ script/createProductPage.js
@@ -5,6 +5,8 @@ let description = document.getElementById("description")
 let button = document.getElementById("buyButton")
 let itemArray
 let itemString
+let finalArray
+let itemRefId
 
 function queryDatabase(collectionName,refId,code) {
     db.collection(collectionName).doc(refId).get().then(code);   
@@ -13,19 +15,24 @@ function queryDatabase(collectionName,refId,code) {
 
 function displayContent(response){
     console.log(response.title)
-    if (typeof(Storage) !== "undefined") {
-        sessionStorage.itemRef += response.id + ","
-        itemString = sessionStorage.itemRef
-        itemArray = itemString.split(",")
-    } else {
-        // Sorry! No Web Storage support..
-      }
+    itemRefId = response.id
+
     image.style.backgroundImage =`url(${response.image})`
     price.innerHTML = "$"+response.price
     description.innerHTML = response.description
 }
 
-function moveToBuyPage() {
 
+
+button.onclick = function() {
+    if (typeof(Storage) !== "undefined") {
+        //adds item to array and returns string as an array
+        sessionStorage.itemRef += itemRefId + ","
+        itemString = sessionStorage.itemRef
+        itemArray = itemString.split(",")
+        itemArray.pop()
+        console.log(itemArray)
+    } else {
+        // Sorry! No Web Storage support..
+      }
 }
-
