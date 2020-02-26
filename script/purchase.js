d39729e410026997cd3f7d305981a29f3cc7a222
angelASC6 <angelmichaelvasquez@gmail.com>
2020-02-26 14:44:39 -0500

    user input is stored in database

--- script/purchase.js
+++ script/purchase.js
@@ -1,7 +1,16 @@
 let itemIntro = document.getElementById("itemNumber")
 let itemString = sessionStorage.itemRef
 let itemArray = itemString.split(",")
+let db = firebase.firestore()
+
 itemArray.pop()
+//get reference for the textboxes
+let billingCountryBox = document.getElementById("billingCountry")
+let billingFullNameBox = document.getElementById("billingFullName")
+let billingAdressBox1 = document.getElementById("billingAdress1")
+let billingAdressBox2 = document.getElementById("billingAdress2")
+let billingZipcodeBox = document.getElementById("billingZipcode")
+let cardNumber = document.getElementById("cardNumber")
 
 
 
@@ -9,5 +18,17 @@ itemIntro.innerHTML = `${itemArray.length} <span id="itemWord" onclick="dropDown
 
 function dropDown() {
     console.log("hey")
+    pushToDatabase()
+}
+
+function pushToDatabase(){
+    db.collection("users").add({
+        "Billing Country": billingCountryBox.value,
+        "Billing Name": billingFullNameBox.value,
+        "Billing Adress Line 1": billingAdressBox1.value,
+        "Billing Adress Line 2": billingAdressBox2.value,
+        "Billing Zip Code": billingCountryBox.value,
+        "Card Number": cardNumber.value
+    })
 }
 
