c875827cb2b3f83e8f90e92cddf3b11fbca7a2d8
angelASC6 <angelmichaelvasquez@gmail.com>
2020-03-02 15:02:24 -0500

    shipping info to database

--- script/purchase.js
+++ script/purchase.js
@@ -13,27 +13,55 @@ let billingAdressBox2 = document.getElementById("billingAdress2")
 let billingZipcodeBox = document.getElementById("billingZipcode")
 let cardNumber = document.getElementById("cardNumber")
 
+let shippingCountryBox = document.getElementById("shippingCountry")
+let shippingFullNameBox = document.getElementById("shippingFullName")
+let shippingAdressBox1 = document.getElementById("shippingAdress1")
+let shippingAdressBox2 = document.getElementById("shippingAdress2")
+let shippingZipcodeBox = document.getElementById("shippingZipcode")
+
 //reference for the checkboxes
 let sameBox = document.getElementById("sameBox")
 let diffrentBox = document.getElementById("diffrentBox")
 
 
 itemIntro.innerHTML = `${itemArray.length} <span id="itemWord" onclick="dropDown()">Items</span> in the cart`
-
+diffrentBox.checked = true
 function dropDown() {
     console.log("hey")
     pushToDatabase()
 }
 
 function pushToDatabase(){
-    db.collection("users").add({
-        "Billing Country": billingCountryBox.value,
-        "Billing Name": billingFullNameBox.value,
-        "Billing Adress Line 1": billingAdressBox1.value,
-        "Billing Adress Line 2": billingAdressBox2.value,
-        "Billing Zip Code": billingCountryBox.value,
-        "Card Number": cardNumber.value
-    })
+    if(diffrentBox.checked){
+        db.collection("users").add({
+            "Billing Country": billingCountryBox.value,
+            "Billing Name": billingFullNameBox.value,
+            "Billing Adress Line 1": billingAdressBox1.value,
+            "Billing Adress Line 2": billingAdressBox2.value,
+            "Billing Zip Code": billingZipcodeBox.value,
+            "Card Number": cardNumber.value,
+            "Shipping Country": shippingCountryBox.value,
+            "Shipping Name": shippingFullNameBox.value,
+            "Shipping Adress Line 1": shippingAdressBox1.value,
+            "Shipping Adress Line 2": shippingAdressBox2.value,
+            "Shipping Zip Code": shippingZipcodeBox.value
+        })
+    }
+    else{
+        db.collection("users").add({
+            "Billing Country": billingCountryBox.value,
+            "Billing Name": billingFullNameBox.value,
+            "Billing Adress Line 1": billingAdressBox1.value,
+            "Billing Adress Line 2": billingAdressBox2.value,
+            "Billing Zip Code": billingZipcodeBox.value,
+            "Card Number": cardNumber.value,
+            "Shipping Country": billingCountryBox.value,
+            "Shipping Name": billingFullNameBox.value,
+            "Shipping Adress Line 1": billingAdressBox1.value,
+            "Shipping Adress Line 2": billingAdressBox2.value,
+            "Shipping Zip Code": billingZipcodeBox.value
+        })
+    }
 }
 
 //runs when check boxes are clicked
